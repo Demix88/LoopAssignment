0d28c5fa3598b8c7faf87e7e7d64b183f412924a
Demix88 <skrasnauskis@gmail.com>
2022-03-08 13:47:54 +0000

    added 2nd part of assignment

--- LoopAssignment/Program.cs
+++ LoopAssignment/Program.cs
@@ -10,6 +10,30 @@ namespace LoopAssignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Type a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number > 15)
+            {
+                Console.WriteLine("Please try again");
+                Console.WriteLine("Please Type a number");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+
+
+            }
+            Console.WriteLine("Select a number for countdown:");
+           
+            int countNum = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(countNum);
+                countNum -- ;
+            }
+            while (countNum > 0);
+            Console.ReadLine();
+            
+
+
         }
     }
 }
